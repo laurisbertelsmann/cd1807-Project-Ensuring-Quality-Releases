1e93eed66ba0ad8302e1ab4eb8aadb1079e37968
laurisbertelsmann <lauris.stiglics@bertelsmann.de>
2023-08-10 16:47:08 +0300

    switchibng to windows webapp!

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,20 +2,22 @@ resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_windows_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
 
-site_config {
+  site_config {
     remote_debugging_enabled = true
     always_on = false
-  
+    application_stack {
+      dotnet_version = "v6.0"
+    }
   }
 
   app_settings = {
